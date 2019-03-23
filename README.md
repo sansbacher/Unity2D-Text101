de11f6b16d67aa9fe87ff84b443cc4b1423f5f55
Saul Ansbacher <45206774+sansbacher@users.noreply.github.com>
2019-03-23 17:58:05 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Text101 module from Udemy's Unity2D course
+(from GameDev.tv)
+
+This was my Text101 project, with several additions.
+
+##"I'll Flyaway"
+
+I'm less into designing and more into programming so I added some PUZZLE / CHALLENGE elements! There's a clock (you only have a time limit), the gate is randomized (you need to discover it), and there's 2 other objectives you must complete before boarding. Good luck!
+
+There's some code and screenshots from the Unity Editor to show what I did. Hopefully you can re-assemble the came from the various bits and pieces if you'd like rebuild it.
+
+Code is probably excessively commented since I often have to take long breaks from working on this course and come back to it later - the extra comments are to remind me what was going on!
+
+There's a clock that increments with each move/change-of-state (you only have 36 mins!), the departure gate is randomized so you have to discover it, and you need to accomplish 2 things before you can board your plane.
+
+(credits for the airport text adventure idea go to an old C++ tutorial for DOS from years ago by Coronado Enterprises I stumbled upon once. But the implementation in Unity using 'States' is mine)
---
+++ code-scripts/State.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Last Updated Jan 6, 2019
+// Unity 2D course, Text 101
+// With added code for a getting the Name of the State, and what Objectives the Player accomplishes by reaching this State, and the amount of time this State adds to the clock
+
+[CreateAssetMenu(menuName = "State")]			// Makes it appear as a Scriptable Object type in Unity (called 'State') - right-click Create... to instantiate more "States"
+public class State : ScriptableObject {         // Using ScriptableObject rather than MonoBehavour to store text and whatnot
+
+	[TextArea(10, 14)][SerializeField] string storyText;            // SerializeField = available in Inspector.	TextArea(minVisibleLines, numLinesBeforeScroll) = TextBox in the Inspector
+	[SerializeField] State[] nextStates;                            // An array of States (where to go next). Unity UI will let you define size and populate
+	[SerializeField] string achievedObjective;                      // What the Player "gets" or "accomplished" by making it to this State
+	[SerializeField] int  minutesForState;							// How much time passes entering this State, Unity defaults to 0
+
+
+	public string GetStateStory() {		// getter
+		return storyText;
+	}
+
+	public State[] GetNextStates() {    // getter
+		return nextStates;
+	}
+
+	public string GetAchievedObjective() {    // getter added - for puzzles
+		return achievedObjective;
+	}
+
+	public int GetStateMinutes() {    // getter added - for game timer
+		return minutesForState;
+	}
+
+	public string GetStateName() {      // A getter added so we can see the name of this Scriptable Object, helpful for debugging
+        return this.name;
+    }
+
+}
